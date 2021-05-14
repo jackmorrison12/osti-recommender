0de7c4ce20c2e8f425fe1ac444e3563d187839b4
jackmorrison <jsm1317@ic.ac.uk>
2021-05-14 16:23:24 +0100

    Improves cosine weighting

--- engines/v2.py
+++ engines/v2.py
@@ -317,11 +317,12 @@ def v2():
                 weighted = []
                 avg_rating = trackset['rating'].mean()
                 for index, row in trackset.iterrows():
-                    total = 4 * (1 - cosine[0][index]) * (1 - cosine[0][index])
-                    # if "spotify" in track_map[row['tid']]:
-                    #     for a in row['artists']:
-                    #         total += ((artist_map[a]/artist_max) *
-                    #                   (max(cosine[0])/6))/len(row['artists'])
+                    total = (4 * (1 - cosine[0][index]) *
+                             (1 - cosine[0][index]) * 10000) - 39980
+                    if "spotify" in track_map[row['tid']]:
+                        for a in row['artists']:
+                            total += ((artist_map[a]/artist_max) *
+                                      (max(cosine[0])/4))/len(row['artists'])
                     combined.append(total)
                     weighted.append(total*(row['rating']**0.6))
 
