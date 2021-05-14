b20da3d3e82032cf3f7c0d75755fb7de1e44ea30
jackmorrison <jsm1317@ic.ac.uk>
2021-05-14 17:11:57 +0100

    Fixes boost for negative values

--- engines/v2.py
+++ engines/v2.py
@@ -352,7 +352,8 @@ def v2():
 
                 for boost in boost_map[str(user)][workout2wid[idx2workout[wid]]]:
                     feedback[trackset[trackset['tid'] == boost['tid']
-                                      ].index.values[0]] += (int(boost['value']) * weight)
+                                      ].index.values[0]] = max(0,feedback[trackset[trackset['tid'] == boost['tid']
+                                      ].index.values[0]]) + (int(boost['value']) * weight)
 
                 v5_max_list = np.argsort(feedback)[::-1]
                 recs = []
